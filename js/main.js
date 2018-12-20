d2d5b92ce93e2a0c5dda992ddac03188b6540521
Programmer_kirill <programmer_kirill@g0ncharuk.local>
2018-12-20 12:55:13 +0200

    commit

--- js/main.js
+++ js/main.js
@@ -15,6 +15,26 @@ $('.call_modal_container_btn').click(function(){
     $('.call_modal').removeClass('active');
     $('.call_modal_container').removeClass('active');
 });
-$('section.blog_comment .wrapper .blog_comment_container_rate .comment_rate_hide').click(function(){
-    $('section.blog_comment .wrapper .modal.active').toggleClass('active');
+$('[data-comment]').click(function(){
+    var comment = $(this).attr('data-comment');
+    $('section.blog_comment .wrapper .modal' + comment).toggleClass('active');
+    if($('section.blog_comment .wrapper .modal' + comment).hasClass('active')){
+        $('[data-comment="' + comment + '"]' + ' .comment_rate_hide_text').html('Скрыть ответы');
+        $('[data-comment="' + comment + '"]' + ' .comment_rate_hide_icon').addClass('active');
+    } else {
+        $('[data-comment="' + comment + '"]' + ' .comment_rate_hide_text').html('Показать ответы');
+        $('[data-comment="' + comment + '"]' + ' .comment_rate_hide_icon').removeClass('active');
+    }
+    console.log('[data-comment="' + comment + '"]' + ' .comment_rate_hide_text');
+    
 });
+// $('section.blog_comment .wrapper .blog_comment_container_rate .comment_rate_hide').click(function(){
+//     $('section.blog_comment .wrapper .modal').toggleClass('active');
+//     if($('section.blog_comment .wrapper .modal').hasClass('active')){
+//         $('.comment_rate_hide_text').html('Скрыть ответы');
+//         $('.comment_rate_hide_icon').addClass('active');
+//     } else {
+//         $('.comment_rate_hide_text').html('Показать ответы');
+//         $('.comment_rate_hide_icon').removeClass('active');
+//     }
+// });
